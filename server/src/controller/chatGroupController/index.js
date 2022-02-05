fdf258ed171a676e2b7483298deb2da621a08b71
fudaosheng <fudaosheng@bytedance.com>
2022-02-05 14:28:27 +0800

    api: search chat group

--- server/src/controller/chatGroupController/index.js
+++ server/src/controller/chatGroupController/index.js
@@ -7,6 +7,7 @@ const { CHAT_GROUPS_TABLE, TABLE_NAMES, CHAT_GROUP_CONTACTS_TABLE, IDENTIRY_LEVE
  */
 
 class ChatGroupController {
+  // 创建群聊
   async createChatGroup(ctx) {
     const { name, avatar } = ctx.request.body;
     const ownerId = ctx.user.userId;
@@ -26,22 +27,40 @@ class ChatGroupController {
         { [CHAT_GROUPS_TABLE.NAME]: name, [CHAT_GROUPS_TABLE.AVATAR]: avatar, [CHAT_GROUPS_TABLE.OWNER_ID]: ownerId },
         TABLE_NAMES.CHAT_GROUPS
       );
-      
-      // 向群组成员表中插入群主身份
-      await ctx.service.dbService.insert({
-        [CHAT_GROUP_CONTACTS_TABLE.GROUP_ID]: chatGroup.insertId, //群id
-        [CHAT_GROUP_CONTACTS_TABLE.USER_ID]: ownerId,
-        [CHAT_GROUP_CONTACTS_TABLE.IDENTITY]: IDENTIRY_LEVEL.OWNER,
-      }, TABLE_NAMES.CHAT_GROUP_CONTACTS);
 
-      return ctx.makeResp({ code: STATUS_CODE.SUCCESS, data: {
-        id: chatGroup.insertId,
-        name,
-        avatar
-      } });
+      // 向群组成员表中插入群主身份
+      await ctx.service.dbService.insert(
+        {
+          [CHAT_GROUP_CONTACTS_TABLE.GROUP_ID]: chatGroup.insertId, //群id
+          [CHAT_GROUP_CONTACTS_TABLE.USER_ID]: ownerId,
+          [CHAT_GROUP_CONTACTS_TABLE.IDENTITY]: IDENTIRY_LEVEL.OWNER,
+        },
+        TABLE_NAMES.CHAT_GROUP_CONTACTS
+      );
+
+      return ctx.makeResp({
+        code: STATUS_CODE.SUCCESS,
+        data: {
+          id: chatGroup.insertId,
+          name,
+          avatar,
+        },
+      });
     } catch (err) {
       return ctx.makeResp({ code: STATUS_CODE.ERROR, message: JSON.stringify(err) });
     }
+  }
+  // 根据ID搜索群
+  async getChatGroupListById(ctx) {
+    const { id } = ctx.request.query;
+    const result = await ctx.service.dbService.query({ [CHAT_GROUPS_TABLE.ID]: id }, TABLE_NAMES.CHAT_GROUPS);
+    return ctx.makeResp({ code: STATUS_CODE.SUCCESS, data: result })
+  }
+  // 根据群名称模糊搜索群
+  async getChatGroupListByName(ctx) {
+    const { name } = ctx.request.query;
+    const result = await ctx.service.dbService.query({ [CHAT_GROUPS_TABLE.NAME]: name }, TABLE_NAMES.CHAT_GROUPS, { isLikeQuery: true });
+    return ctx.makeResp({ code: STATUS_CODE.SUCCESS, data: result })
   }
 }
 
--- server/src/router/chatGroups/index.js
+++ server/src/router/chatGroups/index.js
@@ -6,4 +6,10 @@ const router = new KoaRouter({ prefix: '/api/chat_group' });
 // 创建群组
 router.post('/create', chatGroupController.createChatGroup);
 
+// 根据ID搜索群
+router.get('/get_chat_group_list_by_id', chatGroupController.getChatGroupListById);
+
+// 根据name模糊搜索群
+router.get('/get_chat_group_list_by_name', chatGroupController.getChatGroupListByName);
+
 module.exports = router;
